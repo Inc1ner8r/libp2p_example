2228a82e9590e98e51a030b629dd4324965f3ab2
inciner8r <punarvpawade17@gmail.com>
2023-06-10 23:15:36 +0530

    discovry

--- main.go
+++ main.go
@@ -10,16 +10,30 @@ import (
 	"os/signal"
 	"syscall"
 
+	"sync"
+	"time"
+
 	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
-	ma "github.com/multiformats/go-multiaddr"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	discovery "github.com/libp2p/go-libp2p/p2p/discovery/util"
+	"github.com/multiformats/go-multiaddr"
 )
 
 func main() {
 	masternode()
 }
+
+// DiscoveryInterval is how often we search for other peers via the DHT.
+const DiscoveryInterval = time.Second * 10
+
+// DiscoveryServiceTag is used in our DHT advertisements to discover
+// other peers.
+const DiscoveryServiceTag = "universal-connectivity"
 
 func masternode() {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -29,6 +43,17 @@ func masternode() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Setup DHT with empty discovery peers so this will be a discovery peer for other
+	// peers. This peer should run with a public ip address, otherwise change "nil" to
+	// a list of peers to bootstrap with.
+	dht, err := NewDHT(context.TODO(), ha, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Setup global peer discovery over DiscoveryServiceTag.
+	go Discover(context.TODO(), ha, dht, DiscoveryServiceTag)
 
 	startListener(ctx, ha)
 
@@ -53,7 +78,7 @@ func makeBasicHost() (host.Host, error) {
 	}
 
 	opts := []libp2p.Option{
-		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/9000"),
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/9001"),
 		libp2p.Identity(priv),
 		libp2p.DisableRelay(),
 	}
@@ -63,7 +88,7 @@ func makeBasicHost() (host.Host, error) {
 
 func getHostAddress(ha host.Host) string {
 	// Build host multiaddress
-	hostAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/p2p/%s", ha.ID().Pretty()))
+	hostAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/p2p/%s", ha.ID().Pretty()))
 
 	// Now we can build a full multiaddress to reach this host
 	// by encapsulating both addresses:
@@ -102,4 +127,95 @@ func doEcho(s network.Stream) error {
 	log.Printf("read: %s", str)
 	_, err = s.Write([]byte(str))
 	return err
+}
+
+// Borrowed from https://medium.com/rahasak/libp2p-pubsub-peer-discovery-with-kademlia-dht-c8b131550ac7
+// NewDHT attempts to connect to a bunch of bootstrap peers and returns a new DHT.
+// If you don't have any bootstrapPeers, you can use dht.DefaultBootstrapPeers
+// or an empty list.
+func NewDHT(ctx context.Context, host host.Host, bootstrapPeers []multiaddr.Multiaddr) (*dht.IpfsDHT, error) {
+	var options []dht.Option
+
+	// if no bootstrap peers, make this peer act as a bootstraping node
+	// other peers can use this peers ipfs address for peer discovery via dht
+	if len(bootstrapPeers) == 0 {
+		options = append(options, dht.Mode(dht.ModeServer))
+	}
+
+	// set our DiscoveryServiceTag as the protocol prefix so we can discover
+	// peers we're interested in.
+	options = append(options, dht.ProtocolPrefix("/"+DiscoveryServiceTag))
+
+	kdht, err := dht.New(ctx, host, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = kdht.Bootstrap(ctx); err != nil {
+		return nil, err
+	}
+
+	var wg sync.WaitGroup
+	// loop through bootstrapPeers (if any), and attempt to connect to them
+	for _, peerAddr := range bootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := host.Connect(ctx, *peerinfo); err != nil {
+				fmt.Printf("Error while connecting to node %q: %-v", peerinfo, err)
+				fmt.Println()
+			} else {
+				fmt.Printf("Connection established with bootstrap node: %q", *peerinfo)
+				fmt.Println()
+			}
+		}()
+	}
+	wg.Wait()
+
+	return kdht, nil
+}
+
+// Borrowed from https://medium.com/rahasak/libp2p-pubsub-peer-discovery-with-kademlia-dht-c8b131550ac7
+// Search the DHT for peers, then connect to them.
+func Discover(ctx context.Context, h host.Host, dht *dht.IpfsDHT, rendezvous string) {
+	var routingDiscovery = routing.NewRoutingDiscovery(dht)
+
+	// Advertise our addresses on rendezvous
+	discovery.Advertise(ctx, routingDiscovery, rendezvous)
+
+	// Search for peers every DiscoveryInterval
+	ticker := time.NewTicker(DiscoveryInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+
+			// Search for other peers advertising on rendezvous and
+			// connect to them.
+			peers, err := discovery.FindPeers(ctx, routingDiscovery, rendezvous)
+			if err != nil {
+				panic(err)
+			}
+
+			for _, p := range peers {
+				if p.ID == h.ID() {
+					continue
+				}
+				if h.Network().Connectedness(p.ID) != network.Connected {
+					_, err = h.Network().DialPeer(ctx, p.ID)
+					if err != nil {
+						fmt.Printf("Failed to connect to peer (%s): %s", p.ID, err.Error())
+						fmt.Println()
+						continue
+					}
+					fmt.Println("Connected to peer", p.ID.Pretty())
+				}
+			}
+		}
+	}
 }
